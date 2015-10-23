c018f0e746c9bc285bfcdf823b6b4f16b3ab1198
Thomas Willson <thomas.willson@me.com>
2015-10-22 18:05:31 -0700

    Update import for delay.h

--- arduino/wiring_private.h
+++ arduino/wiring_private.h
@@ -1,40 +1,40 @@
 /*
-  wiring_private.h - Internal header file.
-  Part of Arduino - http://www.arduino.cc/
+ wiring_private.h - Internal header file.
+ Part of Arduino - http://www.arduino.cc/
 
-  Copyright (c) 2005-2006 David A. Mellis
+ Copyright (c) 2005-2006 David A. Mellis
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General
-  Public License along with this library; if not, write to the
-  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
-  Boston, MA  02111-1307  USA
+ You should have received a copy of the GNU Lesser General
+ Public License along with this library; if not, write to the
+ Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ Boston, MA  02111-1307  USA
 
-  $Id: wiring.h 239 2007-01-12 17:58:39Z mellis $
-*/
+ $Id: wiring.h 239 2007-01-12 17:58:39Z mellis $
+ */
 
 #ifndef WiringPrivate_h
 #define WiringPrivate_h
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <avr/delay.h>
+#include <util/delay.h>
 #include <stdio.h>
 #include <stdarg.h>
 
 #include "../arduino/wiring.h"
 
 #ifdef __cplusplus
-extern "C"{
+extern "C" {
 #endif
 
 #ifndef cbi
@@ -53,9 +53,7 @@ extern "C"{
 #define EXTERNAL_INT_6 6
 #define EXTERNAL_INT_7 7
 
-
 #define EXTERNAL_NUM_INTERRUPTS 8
-
 
 typedef void (*voidFuncPtr)(void);
 
